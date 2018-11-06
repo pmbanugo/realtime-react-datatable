171be09cbfbedb471f8988c1efff4f6ec2b9b994
Peter Mbanugo <chuksgerrard@yahoo.com>
2018-11-06 16:02:00 +0100

    Update seed.js
    get token from hamoni api

--- seed.js
+++ seed.js
@@ -1,16 +1,46 @@
-let Hamoni = require("hamoni-sync");
+const Hamoni = require("hamoni-sync");
+const https = require("https");
 
-let hamoni = new Hamoni("AccountID", "APP_ID");
+const accountId = "YOUR_ACCOUNT_ID";
+const appId = "YOUR_APP_ID";
+let hamoni;
 
-hamoni
-  .connect()
-  .then(response => {
+const data = JSON.stringify({ accountId, appId });
+
+const options = {
+  hostname: "api.sync.hamoni.tech",
+  path: "/v1/token",
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+    "Content-Length": data.length
+  }
+};
+
+const req = https.request(options, res => {
+  console.log(`statusCode: ${res.statusCode}`);
+
+  res.on("data", token => {
+    hamoni = new Hamoni(token);
+
     hamoni
-      .createList("datagrid", [
-        { firstName: "James", lastName: "Darwin" },
-        { firstName: "Jimmy", lastName: "August" }
-      ])
-      .then(() => console.log("create success"))
+      .connect()
+      .then(response => {
+        hamoni
+          .createList("datagrid", [
+            { firstName: "James", lastName: "Darwin" },
+            { firstName: "Jimmy", lastName: "August" }
+          ])
+          .then(() => console.log("create success"))
+          .catch(error => console.log(error));
+      })
       .catch(error => console.log(error));
-  })
-  .catch(error => console.log(error));
+  });
+});
+
+req.on("error", error => {
+  console.error(error);
+});
+
+req.write(data);
+req.end();
